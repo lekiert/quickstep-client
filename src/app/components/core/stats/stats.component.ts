a96e361fba2e773bdd20a9af05f3b0a24b1b25ad
Lukasz Ekiert <ekiert.lukasz@gmail.com>
2017-05-02 20:38:42 +0200

    fix

--- src/app/components/core/stats/stats.component.ts
+++ src/app/components/core/stats/stats.component.ts
@@ -71,7 +71,7 @@ export class StatsComponent implements OnInit {
       let id = +params['id'];
       this.id = id;
       if (id) {
-        this.userService.getUser(id).then(user => {
+        this.userService.getUserFromAPI(id, false).then(user => {
           this.student = user
         })
       }
